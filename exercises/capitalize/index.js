b7e173f93691b3d4e0a832cc6f64d89dbc25b327
Anuar Tastembekov <atastembekov@dar.kz>
2018-11-01 14:46:49 +0600

    Capitalize 2 solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,7 +8,17 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    return str.split(' ').map(string => string[0].toUpperCase() + string.slice(1)).join(' ');
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
 }
 
 module.exports = capitalize;
