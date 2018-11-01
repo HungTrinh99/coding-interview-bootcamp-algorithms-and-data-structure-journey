49addf35df87e105ee77188135b70abb185691fa
Anuar Tastembekov <atastembekov@dar.kz>
2018-11-01 11:31:39 +0600

    Max char 1 solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let result = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let i = 0; i < str.length; i++) {
+        result[str[i]] = result[str[i]] + 1 || 1;
+
+        if (Math.max([max, result[str[i]]]) !== max) {
+            max = Math.max([max, result[str[i]]]);
+            maxChar = str[i];
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
