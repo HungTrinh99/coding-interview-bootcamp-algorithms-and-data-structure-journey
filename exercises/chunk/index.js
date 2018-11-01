becd42fa986d632e11e706557e4398abae68a49f
Anuar Tastembekov <atastembekov@dar.kz>
2018-11-01 12:22:24 +0600

    Chunk 3 solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,15 +10,11 @@
 
 function chunk(array, size) {
     let chunked = [];
+    let index =0;
 
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
-
-        if (!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
 
     return chunked;
