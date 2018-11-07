6a95a0afb06d23125362d53c9d056726f4d20f2b
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2018-11-07 18:35:24 +0600

    Fibonacci 1 solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,17 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    const result = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        let a = result[i - 1];
+        let b = result[i - 2];
+
+        result.push(a + b);
+    }
+
+    return result[n];
+}
 
 module.exports = fib;
